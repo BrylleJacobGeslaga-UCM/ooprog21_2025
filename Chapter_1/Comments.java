c9847d7d5947a35524f1fabd844530f09ac060f9
BrylleJacobGeslaga-UCM <92422209+BrylleJacobGeslaga-UCM@users.noreply.github.com>
2025-08-29 15:41:25 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,7 @@
+public class Comments {
+   public static void main(String[] args) {
+      //This is a line comment
+      /*This is a block comment */
+      /**This is a Javadoc comment */
+   }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+   public static void main(String[] args) {
+      System.out.println("Sever the parts of my soul I’ve infected with poisonous cells");
+      System.out.println("Or pass the elastic and rope. With a dull pair of Scissors, I’ll cut. Cut it.");
+      System.out.println("Sever the parts of myself I let fester, and turn into hell");
+      System.out.println("Numb and anaesthetised, wrap me in stitches. I’ll cut it by myself.");
+   }
+}
