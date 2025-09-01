8607b08837b22fc3ccdbbbb04a09277eba74c3a7
BrylleJacobGeslaga-UCM <92422209+BrylleJacobGeslaga-UCM@users.noreply.github.com>
2025-09-01 15:55:33 +0800

    added FixDebugOne file

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,20 @@
+public class FixDebugOne
+{
+   /* This program displays a greeting */
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+   }
+}
+/*
+List down the spotted bugs below:
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. Wrong syntax for class name "FixDebug One" instead of "FixDebugOne"
+2. Wrong syntax, missing ';' after System.out.println("Hello World!")
+3. Wrong syntax for identifier "Systm" instead of "System"
+4. Wrong syntax for method name "Main" instead of "main"
+5. Wrong syntax for parameter "String args" instead of "String[] args"
+6. Missing closing '}' for class
+*/
