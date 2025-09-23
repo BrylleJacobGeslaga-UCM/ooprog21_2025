748934abe363b736b0a3edf5f86d6dd016a30318
BrylleJacobGeslaga-UCM <92422209+BrylleJacobGeslaga-UCM@users.noreply.github.com>
2025-09-24 06:18:16 +0800

    Add DebugCircle java file

---
+++ Chapter_4/DebugCircle.java
@@ -0,0 +1,24 @@
+public class DebugCircle {
+
+   private int radius;
+   private int diameter;
+   private final double PI = 3.14159;
+   private double area;
+   
+   public DebugCircle(int r) {
+
+      radius = r;
+      diameter = 2 * r;
+      area = PI * r * r;
+   }
+
+   public int getRadius() { //Remove semi colon
+      return radius; //changed radiuss to radius
+   }
+   public int getDiameter() { //changed void return type to int
+      return diameter;
+   }
+   public double getArea() {
+      return area;
+   }
+}
